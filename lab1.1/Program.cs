0c9117cf1495e4215351f024b0c68c1475594693
Илья Абрамёнок <i.abramenok@slata.com>
2022-06-06 18:13:50 +0800

    Добавьте файлы проекта.

---
+++ lab1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Привет, подсказать текущую дату,время? \n 1) Да \n 2) Нет");
+            int output1= int.Parse(Console.ReadLine());
+            if (output1 == 1)
+            {
+                Console.WriteLine(DateTime.Now);
+            }
+            if (output1 == 2)
+            {
+                Console.WriteLine("Всего хорошего");
+            }
+
+
+        }
+    }
+}
